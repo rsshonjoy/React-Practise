d4a8d64e5018cb70ecab3bf1fda8b17d37e9b8ea
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-23 21:33:37 +0600

    add array element in react

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const domContainer = document.querySelector("#root");
 
-const myElement = React.createElement("div", null, "RS Shonjoy")
+const myElement = React.createElement("div", null, [
+  "RS Shonjoy",
+  React.createElement("p", null, "Hello world"),
+  React.createElement("p", null, "Hello world")
+])
 
 ReactDOM.render(myElement, domContainer);
